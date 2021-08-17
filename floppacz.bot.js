12349c97dcf988143b5ce5bea8c9c04534b5fc81
Mocar2D <69865499+Mocar2D@users.noreply.github.com>
2021-08-18 00:45:21 +0200

    Updated floppacz-bot.js to discord.js v13, new commands
    added support for discord.js version 13
    added new commands: 
    - floppa help
    - floppa ping
    - little help when pinging the bot

--- floppacz.bot.js
+++ floppacz.bot.js
@@ -1,30 +1,61 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,] });
+
 
 var fs = require('fs');
 var files = fs.readdirSync('./floppa/');
 
 
-bot.on('ready', () => {
-  bot.user.setStatus('dnd') //You can set idle, dnd or invisible
-  bot.user.setActivity("use floppa to get a image!",  { type: 'PLAYING' }) // PLAYING , LISTENING , WATCHING , STREAMING
+
+
+client.on('ready', () => {
+  client.user.setStatus('invisible') //You can set idle, dnd or invisible
+  client.user.setActivity("use floppa to get a image!",  { type: 'PLAYING' }) // PLAYING , LISTENING , WATCHING , STREAMING
+  console.log("on discord lol");
 });
 
-bot.on('message', msg => {
+
+client.on('message', msg => {
     if (msg.content === 'floppa') {
         //number = 29;
         //imageNumber = Math.floor (Math.random() * (number -1 + 1))+ 1;
         //msg.channel.send ( {files: ["./floppa/" + imageNumber + ".png"]} )
         imageNumber = files[Math.floor(Math.random()*files.length)]
-        msg.channel.send ( {files: ["./floppa/" + imageNumber]} ) 
+        msg.channel.send ( {files: ["./floppa/" + imageNumber]} )
 var now = new Date();
 // convert date to a string in UTC timezone format:
 console.log(now.toLocaleTimeString());
-// Output: Wed, 21 Jun 2017 09:13:01 GMT
+// Output: 22:44:34
     }
 
   });
+ 
+  client.on('message', message => {
+    if (message.mentions.has(client.user)) {
+        message.channel.send('You can get help by saying "floppa help" in the chat :wink:');
+    }
+});
+//sends a little message when pinged
+client.on('message', message => {
+  if (message.content === 'floppa ping') {  
+    message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+  }
+});
+// just a ping command 
 
+client.on('message', message => {
+  if (message.content === 'floppa help') {
+    message.channel.send({ embeds: [helpEmbed] });
+  }
+});
 
+//sends an embed of commands after typing floppa help
+  
+const helpEmbed = new MessageEmbed()
+	.setColor('0x00FFFF ')
+	.setTitle('Commands')
+	.setDescription('floppa - displays an image of a floppa\nfloppa ping - shows the ping of the bot and the api\nfloppa help - well you are using it right now')
 
-bot.login ('TOKEN');
+// the floppa help embed itself
+
+client.login ('TOKEN');
